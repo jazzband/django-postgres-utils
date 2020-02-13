d6be07fc281d6a00def2035e49bfcc8811b843e7
Andreas Hasenkopf <ahasenkopf@suse.de>
2020-02-13 09:54:00 +0100

    Removed temp. app

--- src/src/settings.py
+++ src/src/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'postgres_utils.apps.PostgresUtilsConfig',
     'testapp.apps.TestappConfig'
 ]
