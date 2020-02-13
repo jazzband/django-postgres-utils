9950446c56c763f7fc062a1c0be3fabb93f1ca41
Andreas Hasenkopf <crazyscientist@users.noreply.github.com>
2020-02-13 11:31:25 +0100

    Renamed PyPI package to `django-postgres-tweaks` (#4)
    * Renamed PyPI package to `django-postgres-tweaks`
    * Updated setup.py

--- setup.py
+++ setup.py
@@ -19,15 +19,17 @@ with open("README.md") as fh:
 
 
 setup(
-    name='django-postgres-utils',
+    name='django-postgres-tweaks',
     version='0.1.0',
     description='Special PostgreSQL lookups and functions for Django apps',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Andreas Hasenkopf',
-    author_email='ahasenkopf@suse.com',
+    author_email='andreas@hasenkopf.xyz',
     url='https://github.com/Canned-Django/django-postgres-utils',
-    download_url='https://github.com/Canned-Django/django-postgres-utils/archive/master.zip',
+    project_urls={
+        'Documentation': 'https://canned-djangodjango-postgres-utils.readthedocs.io/'
+    },
     packages=["postgres_utils"],
     package_dir={'': 'src'},
     license='MIT',
