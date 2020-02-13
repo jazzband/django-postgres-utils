15788c47e068ca429de6860dcd1005904a8a6aec
Andreas Hasenkopf <crazyscientist@users.noreply.github.com>
2020-02-13 13:35:37 +0100

    Fixed issues with Travis CI deployment (#5)
    * Change PyPI token
    * Version 0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Andreas Hasenkopf'
 author = 'Andreas Hasenkopf'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
